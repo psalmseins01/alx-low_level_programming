090fbac5cde1ca5fedc6d958224ec75c2f2080d3
Anthony Gbobo <psalmseins@gmail.com>
2023-09-20 16:37:24 -0700

    Function that prints every other character

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+* puts2 - print even numbers
+* @str: function parameter
+* Return: return void
+*/
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+	}
+	_putchar('\n');
+}
