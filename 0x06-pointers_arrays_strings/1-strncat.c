20f955f89762df2fd54c5f22079a06a517f312bf
Anthony Gbobo <psalmseins@gmail.com>
2023-09-21 17:27:14 -0700

    Concatenate strings of n lenght

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+/**
+* _strncat - concatenate two strings together
+* @dest: function parameter pointer dest
+* @src: function parameter pointer src
+* @n: parameter n
+* Return: return dest
+*/
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int len = strlen(dest);
+	int i;
+
+	for (i = 0; i < n && *src != '\0'; i++)
+	{
+		dest[len + i] = src[i];
+		src++;
+	}
+	return (dest);
+}
