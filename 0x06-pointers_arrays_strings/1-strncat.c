5ff42224b6bf1688b07c0c0faa1dd04f5dd45e66
Anthony Gbobo <psalmseins@gmail.com>
2023-09-21 17:42:07 -0700

    Concatenate strings correction

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -16,8 +16,9 @@ char *_strncat(char *dest, char *src, int n)
 
 	for (i = 0; i < n && *src != '\0'; i++)
 	{
-		dest[len + i] = src[i];
+		dest[len + i] = *src;
 		src++;
 	}
+	dest[len + i] = '\0';
 	return (dest);
 }
