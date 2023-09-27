f885a4b03351e3bfbe5faab4e481c9b5b8e72a9b
Anthony Gbobo <psalmseins@gmail.com>
2023-09-26 17:46:34 -0700

    Assign value to a string

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <string.h>
+/**
+ * set_string -  function that sets a string
+ * @s: string to set
+ * @to: set value to to
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
