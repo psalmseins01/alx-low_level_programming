b8fb717ea5798dcc265e57063ac92c3d8cd1390c
Anthony Gbobo <psalmseins@gmail.com>
2023-10-07 16:55:32 -0700

    Function that multiplies two numbers

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -2,35 +2,29 @@
 #include <ctype.h>
 #include <stdlib.h>
 /**
- * main - function to add positive numbers
+ * main - function to multiply two numbers
  * @argc: argument count
  * @argv: argument vector
  * Return: return 0
  */
+
 int main(int argc, char *argv[])
 {
-	int i, j;
-	int sum = 0;
+	int num_1, num_2 = 0;
+	int total;
 
-	if (argc == 1)
+	if (argc != 3)
 	{
-		printf("%d\n", 0);
+		printf("Error\n");
+		return (1);
 	}
 	else
 	{
-		for (i = 1; i < argc; i++)
-		{
-			for (j = 0; argv[i][j] != '0'; j++)
-			{
-				if (!(isdigit(argv[i][j])))
-				{
-					printf("Error");
-					return (1);
-				}
-				sum = sum + atoi(argv[i]);
-			}
-		}
-		printf("%d\n", sum);
+		num_1 = atoi(argv[1]);
+		num_2 = atoi(argv[2]);
+
+		total = num_1 * num_2;
+		printf("%d\n", total);
 	}
 	return (0);
 }
