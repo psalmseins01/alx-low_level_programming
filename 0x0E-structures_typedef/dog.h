9f7cc824aa9ef75abe8a973cbcb7a642e3bd0e62
Anthony Gbobo <psalmseins@gmail.com>
2023-10-11 18:24:55 -0700

    Define a structure called dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - structure named dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: name of the owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
