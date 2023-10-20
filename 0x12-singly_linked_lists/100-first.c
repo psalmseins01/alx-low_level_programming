f7d714dfad86bc2d65b284cb293ad7c360499836
Anthony Gbobo <psalmseins@gmail.com>
2023-10-20 16:12:04 -0700

    Print string before main function

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,9 @@
+#include "lists.h"
+/**
+ * first - executed before the main function
+ */
+void __attribute__ ((constructor)) first()
+{
+	printf("You're beat! and yet, you must allow");
+	printf(",\nI bore my house upon my back!\n");
+}
