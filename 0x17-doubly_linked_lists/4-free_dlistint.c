4d63c59316295a80a8050b25b456c01d368ab080
Anthony Gbobo <psalmseins@gmail.com>
2023-12-07 18:15:59 -0800

    Function that frees a dlistint_t list

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * free_dlistint - Function that frees a dlistint_t list
+ * @head: pointer to head node
+ * Return: return void
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *tmp;
+
+	if (head != NULL)
+		while (head->prev != NULL)
+			head = head->prev;
+
+	while ((tmp = head) != NULL)
+	{
+		head = head->next;
+		free(tmp);
+	}
+}
--- 0x17-doubly_linked_lists/git.sh
+++ 0x17-doubly_linked_lists/git.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/bash
-git add . && git commit -m "Function that adds a new node at the end of a dlistint_t list" && git push
+git add . && git commit -m "Function that frees a dlistint_t list" && git push
